4fe8537b87aa8ecdf376a7f85f1eb0a1305ccfe4
Daniel Mafra <danielmafradev@gmail.com>
2022-03-26 17:29:33 -0300

    feat: create middleware to permissions

---
+++ src/middlewares/permissions/ensurePermission.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { decode, verify } from 'jsonwebtoken';
+import { getUserService } from '../../modules/users/services/getUserService';
+
+function can(role: string) {
+  const roleAuthorized = async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.userId;
+    const hasUser = await getUserService.findOne(userId);
+
+    if (!hasUser) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const userPermissions = hasUser.permissions.split(",");
+
+    if (!userPermissions.includes(role)) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return next();
+  }
+
+  return roleAuthorized;
+}
+
+export { can };
