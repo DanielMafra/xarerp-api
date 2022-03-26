c4fb17a8157db2e467742085dcd691b1fc029356
Daniel Mafra <danielmafradev@gmail.com>
2022-03-26 17:40:46 -0300

    feat: add routes to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import express, { Request, Response, ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import { ensureAuthenticated, validateToken } from './middlewares/auth/ensureAuthenticated';
+import * as authenticateUserController from './modules/users/controllers/authenticateUserController';
+
+import apiRoutes from './routes';
+
 dotenv.config();
 
 const server = express();
@@ -13,6 +18,11 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
+server.post('/login', authenticateUserController.authenticate);
+server.post('/validate', validateToken)
+
+server.use(ensureAuthenticated());
+server.use(apiRoutes);
 
 server.use((req: Request, res: Response) => {
   res.status(404).json({ error: 'Not found' });
