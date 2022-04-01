d8232b8153929b93a5f2e2a482d6fae363a9c80d
Daniel Mafra <danielmafradev@gmail.com>
2022-04-01 17:02:41 -0300

    feat: remove Sellers Dashboard service

--- src/modules/dashboard/services/getDashboardService.ts
+++ src/modules/dashboard/services/getDashboardService.ts
@@ -141,31 +141,4 @@ export const getDashboardService = {
       }
     });
   },
-
-  //SELLERS
-  findSellers: async (minDate: string) => {
-    return await prisma.sale.findMany({
-      where: {
-        updated_at: {
-          gte: new Date(minDate)
-        }
-      },
-      select: {
-        seller: {
-          select: {
-            user: {
-              select: {
-                name: true
-              }
-            }
-          }
-        },
-        unity: {
-          select: {
-            name: true
-          }
-        }
-      }
-    });
-  },
 }
